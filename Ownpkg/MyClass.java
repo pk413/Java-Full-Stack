d71cd895f715d14aa324a3d5ebf1cf1ad0b91b6f
pk413 <121785097+pk413@users.noreply.github.com>
2023-01-02 11:51:14 +0530

    hello world
    hello world program
    methods
    object

---
+++ Ownpkg/MyClass.java
@@ -0,0 +1,24 @@
+package Ownpkg;
+/**
+ * 
+ * @author pavan
+ *
+ */
+
+public class MyClass {
+	/**
+	 * 
+	 * @param args
+	 */
+	public void Mymethod() {
+		System.out.println("I'm in main method");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!!!");
+		System.out.println("I'm pavan");
+		MyClass o = new MyClass();
+		o.Mymethod();
+	}
+
+}
