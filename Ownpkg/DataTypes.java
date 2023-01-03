7ea3a17932919fbd27c32f1ba45a069e18f4603f
pk413 <121785097+pk413@users.noreply.github.com>
2023-01-03 11:04:59 +0530

    Data types
    byte
    short
    int
    long
    float
    double
    char
    boolean

---
+++ Ownpkg/DataTypes.java
@@ -0,0 +1,40 @@
+package Ownpkg;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte b = 0; // byte varies from -128 to 127
+		byte a = 127;
+		System.out.println("a="+a);
+		
+		short c = 0; // short varies from -32768 to 32767
+		short d = 327;
+		System.out.println("d="+d);
+		
+		int e = 0; // integer varies from
+		int f = 759348516;
+		System.out.println("f="+f);
+		
+		long g = 0; // long integer varies from 
+		long h = 9856741358L;
+		System.out.println("h="+h);
+		
+		float i = 0; // float stores upto 7-9 digits
+		float j = 5.67598491649614963941681986965f;
+		System.out.println("j="+j);
+		
+		double k = 0; // double holds the values upto 15-17 digits
+		double m = 4.35987d;
+		System.out.println("m="+m);
+		
+		char n = 'A'; // char varies from 0 to 65536
+		System.out.println("n="+n);
+		
+		boolean o = true;
+		boolean p = false;
+		System.out.println("p="+p);
+		
+	}
+
+}
