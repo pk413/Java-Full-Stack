8bd91efb7f11ee0cbc5c77d901ac68c01b25945f
pk413 <121785097+pk413@users.noreply.github.com>
2023-01-17 09:35:00 +0530

    exercise 16
    exercise 16

---
+++ Hello/World/8645_pavan_kalyan_p_exercise16.java
@@ -0,0 +1,22 @@
+package Hello.World;
+	
+
+public class Hello_World {
+	Hello_World(){
+		System.out.println("Creating default constructor");
+	}
+
+	public static void main(String[] args) {
+		Hello_World o = new Hello_World();
+	
+
+	}
+}
+
+
+/* OUTPUT 
+
+Creating default constructor
+
+
+*/
