ecf5c399b8fcbc4864cf0c2c20eb92b1ba94b6bd
pk413 <121785097+pk413@users.noreply.github.com>
2023-02-23 12:11:50 +0530

    Delete 8645_pavan_kalyan_p_dbms_activityorassognment directory

--- 8645_pavan_kalyan_p_dbms_activityorassognment/8645_pavan_kalyan_p_LABACTIVITY_customers.sql
+++
@@ -1,6 +0,0 @@
-use edubridge;
-select * from customers;
-select * from customers order by country;
-SELECT COUNT(CustomerID), Country FROM Customers GROUP BY Country;
-SELECT COUNT(CustomerID), Country FROM Customers GROUP BY Country HAVING COUNT(CustomerID) > 5;
-SELECT COUNT(CustomerID), Country FROM Customers GROUP BY Country HAVING COUNT(CustomerID) > 5 ORDER BY COUNT(CustomerID) DESC;
--- 8645_pavan_kalyan_p_dbms_activityorassognment/8645_pavan_kalyan_p_LABACTIVITY_view.sql
+++
@@ -1,8 +0,0 @@
-select * from film;
- create view film_view as select title, rental_duration, length from film; 
- select * from film_view;
- update film_view set length=90 where title='academy dinosaur';
- create view customer_view as select customer_id,store_id,first_name,address_id from customer where customer_id is not null with check option;
- select * from customer_view;
- -- delete from customer_view where address_id=5;
- drop view customer_view;
